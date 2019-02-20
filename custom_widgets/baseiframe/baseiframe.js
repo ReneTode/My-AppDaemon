5da9ade21733a4fa4da0521c0d2f5a5ca5dc7e89
Rene Tode <github@reot.org>
2019-02-20 21:43:23 +0100

    added stream option

--- custom_widgets/baseiframe/baseiframe.js
+++ custom_widgets/baseiframe/baseiframe.js
@@ -90,10 +90,9 @@ function baseiframe(widget_id, url, skin, parameters)
         {
             url_part3 = "&api_password=" + self.parameters.password;
         }
-        if ("token" in self.parameters)
+        if ("stream" in self.parameters)
         {
-            url_part2 =  "/api/camera_proxy_stream/"+ self.parameters.camera
-            url_part3 = "?token=" + self.parameters.token;
+            url_part2 = url_part2.replace("camera_proxy","camera_proxy_stream");
         }
         url = url_part1 + url_part2 + url_part3;
         view_camera(self, url)
